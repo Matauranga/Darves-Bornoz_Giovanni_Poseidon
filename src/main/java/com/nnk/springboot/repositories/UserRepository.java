68437df3fe72c0dbe2913b72499e7732a9536bac
Giovanni Darves-Bornoz <gio.db@hotmail.fr>
2023-09-14 13:37:39 +0200

    Add @Repository annotation

--- src/main/java/com/nnk/springboot/repositories/UserRepository.java
+++ src/main/java/com/nnk/springboot/repositories/UserRepository.java
@@ -3,8 +3,9 @@ package com.nnk.springboot.repositories;
 import com.nnk.springboot.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
-
-public interface UserRepository extends JpaRepository<User, Integer>, JpaSpecificationExecutor<User> {
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer>, JpaSpecificationExecutor<User> {//TODO : inspect
 
 }
